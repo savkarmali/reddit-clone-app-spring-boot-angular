ac60cfcf04291e778effbac43aef2bbad04f2053
savkarmali <savkarmali3@gmail.com>
2026-03-10 00:17:28 +0530

    Initial comment

---
+++ spring-boot-reddit-clone/src/main/java/com/sau/SpringBootRedditCloneApplication.java
@@ -0,0 +1,14 @@
+package com.sau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedditCloneApplication.class, args);
+		System.out.println("App Started...");
+	}
+
+}
---
+++ spring-boot-reddit-clone/src/main/java/com/sau/model/Subreddit.java
@@ -0,0 +1,30 @@
+package com.sau.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Subreddit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    private User user;
+}
